d05ceb84cbc5dd93ee28ed65c87ecb5f38ddba2a
rakesh-mijar <rakeshkumarmijar@gmail.com>
2024-02-20 23:10:13 +0530

    Command Line Arguments Task is Updated

--- DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/commandLineArguments.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/commandLineArguments.java
@@ -1,12 +1,28 @@
-//package branches.blocks;
-//
-//import java.util.Scanner;
-//
-//public class commandLineArguments {
-//    public static void main(String[] args){
-//        Scanner scanner = new Scanner(System.in);
-//        if(args.length ==  0){
-//            sout
-//        }
-//    }
-//}
+package branches.blocks;
+
+import java.util.Scanner;
+
+public class commandLineArguments {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        if(args.length ==  0){
+            System.out.println("Enter the input as arguments");
+        }
+        for(String input:args){
+            switch(input.toLowerCase()) {
+                case "health":
+                    System.out.println(input + " insurance is provided by LIC Insurance");
+                    break;
+                case "vehicle":
+                    System.out.println(input + " insurance is provided by Mahindra Insurance");
+                    break;
+                case "life":
+                    System.out.println(input + " insurance is provided by Bheema Insurance");
+                    break;
+                case "property":
+                    System.out.println(input + " insurance is provided by Madhura Insurance");
+                    break;
+            }
+        }
+    }
+}
