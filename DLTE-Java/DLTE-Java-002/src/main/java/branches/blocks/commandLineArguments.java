c544a1b16e7befade420cb83bae0df63d14c7177
rakesh-mijar <rakeshkumarmijar@gmail.com>
2024-02-20 21:00:06 +0530

    Tax pay based on slabs is updated

--- DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/commandLineArguments.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/commandLineArguments.java
@@ -1,10 +1,12 @@
-package branches.blocks;
-
-import java.util.Scanner;
-
-public class commandLineArguments {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-    }
-}
+//package branches.blocks;
+//
+//import java.util.Scanner;
+//
+//public class commandLineArguments {
+//    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        if(args.length ==  0){
+//            sout
+//        }
+//    }
+//}
--- DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/taxPay.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/taxPay.java
@@ -4,39 +4,60 @@ import java.util.Scanner;
 
 public class taxPay {
     public static void main(String[] args){
+        Double salaryAmount=0.0;
         Scanner scanner = new Scanner(System.in);
-        long salary;
-        System.out.println("Enter salary amount");
-        salary = sc.nextLong();
-        int slabs;
-        System.out.println("Enter the slabs(old=0/new=1)");
-        slabs=sc.nextInt();
-        switch(slabs){
-            case 0:if(salary>=0 && salary<=250000){
-                System.out.println(salary);
-            }
-            else if(salary>250000 && salary<=500000){
-                salary = (long)(salary*0.005);
-                System.out.println(salary);
-            }
-            else if(salary>500000 && salary<=750000){
-                salary = (long)(salary*0.20);
-                System.out.println(salary);
-            }
-            else if(salary>750000 && salary<=1000000){
-                salary = (long)(salary*0.20);
-                System.out.println(salary);
-            }
-            else if(salary>1000000 && salary<=1250000){
-                salary = (long)(salary*0.30);
-                System.out.println(salary);
-            }
-            else if(salary>1250000 && salary<=1500000){
-                salary = (long)(salary*0.30);
-                System.out.println(salary);
-            }
-            break;
-            case 1:
+        System.out.println("Enter your salary");
+        salaryAmount = scanner.nextDouble();
+        System.out.println("enter your choice to calculate tax");
+        System.out.println("0->old slab"+"\n"+"1->new slab");
+        int choice=scanner.nextInt();
+        switch(choice){
+            case 0://calculation of tax based on old slab
+                if(salaryAmount<=250000){
+                    System.out.println("No tax to be paid");
+                }
+                else if(salaryAmount>250000 && salaryAmount<=500000){
+                    System.out.println("tax to be paid is "+(salaryAmount*0.05));
+                }
+                else if(salaryAmount>500000 && salaryAmount<=750000){
+                    System.out.println("Tax to be paid is "+(salaryAmount*0.2));
+                }
+                else if(salaryAmount>750000 && salaryAmount<=1000000){
+                    System.out.println("Tax to be paid is "+(salaryAmount*0.2));
+                }
+                else if(salaryAmount>1000000 && salaryAmount<=1250000){
+                    System.out.println("Tax to be paid is "+salaryAmount*0.3);
+                }
+                else if(salaryAmount>1250000 && salaryAmount<=1500000){
+                    System.out.println("Tax to be paid is "+salaryAmount*0.3);
+                }
+                else{
+                    System.out.println("Tax to be paid is "+salaryAmount*0.3);
+                }
+                break;
+            case 1://calculation of tax based on new slab
+                if(salaryAmount<=250000){
+                    System.out.println("No tax to be paid");
+                }
+                else if(salaryAmount>250000 && salaryAmount<=500000){
+                    System.out.println("tax to be paid is "+(salaryAmount*0.05));
+                }
+                else if(salaryAmount>500000 && salaryAmount<=750000){
+                    System.out.println("Tax to be paid is "+salaryAmount*0.2);
+                }
+                else if(salaryAmount>750000 && salaryAmount<=1000000){
+                    System.out.println("Tax to be paid is "+salaryAmount*0.2);
+                }
+                else if(salaryAmount>1000000 && salaryAmount<=1250000){
+                    System.out.println("Tax to be paid is "+salaryAmount*0.3);
+                }
+                else if(salaryAmount>1250000 && salaryAmount<=1500000){
+                    System.out.println("Tax to be paid is "+salaryAmount*0.3);
+                }
+                else{
+                    System.out.println("Tax to be paid is "+salaryAmount*0.3);
+                }
+                break;
         }
     }
 }
