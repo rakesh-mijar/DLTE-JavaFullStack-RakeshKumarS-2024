84949329157855fb93d02f408f348d6113ae61b5
rakesh-mijar <rakeshkumarmijar@gmail.com>
2024-02-20 16:48:42 +0530

    Minimum balance task updated

--- DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/minimumBalance.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/minimumBalance.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class minimumBalance {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double[] customerBalance = new double[20];
-        for (int i = 0; i < customerBalance.length; i++) {
+        double[] customerBalance = new double[20];//initialize an array for 20 customers
+        for (int i = 0; i < customerBalance.length; i++) {//enter the balance amount for 20 customers
             System.out.println("enter the balance for customer " + (i + 1));
             customerBalance[i] = scanner.nextDouble();
         }
-        minimumBalance.updatedBalances(customerBalance);
+        minimumBalance.updatedBalances(customerBalance);//call the update function defined outside the main
         System.out.println("Updated customer balances ");
-        for (int i = 0; i < customerBalance.length; i++) {
+        for (int i = 0; i < customerBalance.length; i++) {//print the updated value of balance within the main method
             System.out.println("enter the balance for customer " + (i + 1)+" "+ customerBalance[i]);
         }
 
     }
-    static void updatedBalances(double[] bal){
+    static void updatedBalances(double[] bal){ //method to update the customer balance
         for(int i=0;i<bal.length;i++){
             double currentBalance=bal[i];
             if(currentBalance<10000) {
-                if (currentBalance >= 5000 && currentBalance <= 9999) {
+                if (currentBalance >= 5000 && currentBalance <= 9999) {//charges of 3% of their balance between 5000 to 9999
                     bal[i] -= currentBalance * 0.3;
-                }else if (currentBalance >= 1000 && currentBalance <= 4999) {
+                }else if (currentBalance >= 1000 && currentBalance <= 4999) {//charges 5% of their balance between 1000 to 4999
                     bal[i] -= currentBalance * 0.5;
                 }
             }
--- DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/taxPay.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/branches/blocks/taxPay.java
@@ -1,18 +1,42 @@
-//package branches.blocks;
-//
-//import java.util.Scanner;
-//
-//public class taxPay {
-//    public static void main(String[] args){
-////        Scanner scanner = new Scanner(System.in);
-////        long salary;
-////        System.out.println("Enter salary amount");
-////        salary = sc.nextLong();
-////        int slabs;
-////        System.out.println("Enter the slabs(old=0/new=1)");
-////        slabs=sc.nextInt();
-////        switch(slabs){
-//
-//        }
-//    }
-//}
+package branches.blocks;
+
+import java.util.Scanner;
+
+public class taxPay {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        long salary;
+        System.out.println("Enter salary amount");
+        salary = sc.nextLong();
+        int slabs;
+        System.out.println("Enter the slabs(old=0/new=1)");
+        slabs=sc.nextInt();
+        switch(slabs){
+            case 0:if(salary>=0 && salary<=250000){
+                System.out.println(salary);
+            }
+            else if(salary>250000 && salary<=500000){
+                salary = (long)(salary*0.005);
+                System.out.println(salary);
+            }
+            else if(salary>500000 && salary<=750000){
+                salary = (long)(salary*0.20);
+                System.out.println(salary);
+            }
+            else if(salary>750000 && salary<=1000000){
+                salary = (long)(salary*0.20);
+                System.out.println(salary);
+            }
+            else if(salary>1000000 && salary<=1250000){
+                salary = (long)(salary*0.30);
+                System.out.println(salary);
+            }
+            else if(salary>1250000 && salary<=1500000){
+                salary = (long)(salary*0.30);
+                System.out.println(salary);
+            }
+            break;
+            case 1:
+        }
+    }
+}
