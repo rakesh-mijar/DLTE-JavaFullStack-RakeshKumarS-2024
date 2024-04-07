93e8ad4439f4d322dcca59ed37c24a03164e4017
rakesh-mijar <rakeshkumarmijar@gmail.com>
2024-04-07 14:32:05 +0530

    Final Project Updated

--- DLTE-FINAL-PROJECT/DLTE-PROJECT-DAO1/src/main/java/com/example/dao1/remotes/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dao1.remotes;
-
-import com.example.dao1.entities.Accounts;
-
-import java.util.List;
-
-public interface AccountRepository {
-    List<Accounts> filterByStatus(Accounts accounts);
-}
